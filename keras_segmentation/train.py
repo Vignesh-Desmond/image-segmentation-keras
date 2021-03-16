918ef8a881fbccb3351268c13e94b0b1afd783f6
SP <66484763+splion-360@users.noreply.github.com>
2021-03-16 23:37:30 +0530

    Update train.py

--- keras_segmentation/train.py
+++ keras_segmentation/train.py
@@ -52,7 +52,7 @@ def weighted_categorical_crossentropy(weights):
     # weights = [0.9,0.05,0.04,0.01]
     def wcce(y_true, y_pred):
         Kweights = K.constant(weights)
-        if not K.is_tensor(y_pred): y_pred = K.constant(y_pred)
+        if not K.is_keras_tensor(y_pred): y_pred = K.constant(y_pred)
         y_true = K.cast(y_true, y_pred.dtype)
         return K.categorical_crossentropy(y_true, y_pred) * K.sum(y_true * Kweights, axis=-1)
     return wcce
