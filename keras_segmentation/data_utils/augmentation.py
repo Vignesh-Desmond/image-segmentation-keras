3dd9fb450429dbc660fe3f493c0afdcce9b7974a
SP <66484763+splion-360@users.noreply.github.com>
2021-03-17 16:30:51 +0530

    Update augmentation.py

--- keras_segmentation/data_utils/augmentation.py
+++ keras_segmentation/data_utils/augmentation.py
@@ -21,18 +21,17 @@ def _load_augmentation_aug_geometric():
                        pad_mode='constant',
                        pad_cval=(0, 255)),
         iaa.Crop(percent=(0.0, 0.1)),
-        iaa.Crop(percent=(0.3, 0.5)),
-        iaa.Crop(percent=(0.3, 0.5)),
-        iaa.Crop(percent=(0.3, 0.5)),
+        iaa.Crop(percent=(0.1, 0.2)),
+        iaa.Crop(percent=(0.1, 0.2)),
+        iaa.Crop(percent=(0.0, 0.1)),
         iaa.Sequential([
             iaa.Affine(
                     # scale images to 80-120% of their size,
                     # individually per axis
-                    scale={"x": (0.8, 1.2), "y": (0.8, 1.2)},
+                    scale={"x": (0.5, 1), "y": (0.5, 1)},
                     # translate by -20 to +20 percent (per axis)
                     translate_percent={"x": (-0.2, 0.2), "y": (-0.2, 0.2)},
-                    rotate=(-45, 45),  # rotate by -45 to +45 degrees
-                    shear=(-16, 16),  # shear by -16 to +16 degrees
+               
                     # use nearest neighbour or bilinear interpolation (fast)
                     order=[0, 1],
                     # if mode is constant, use a cval between 0 and 255
@@ -41,7 +40,7 @@ def _load_augmentation_aug_geometric():
                     # use any of scikit-image's warping modes
                     # (see 2nd image from the top for examples)
             ),
-            iaa.Sometimes(0.3, iaa.Crop(percent=(0.3, 0.5)))])
+            iaa.Sometimes(0.3, iaa.Crop(percent=(0.1,0.2)))])
     ])
 
 
