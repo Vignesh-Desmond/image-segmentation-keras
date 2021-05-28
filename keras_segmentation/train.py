777343b9cd0f8040944d3d8c34826de812cec847
SP <66484763+splion-360@users.noreply.github.com>
2021-05-28 13:45:12 +0530

    Update train.py

--- keras_segmentation/train.py
+++ keras_segmentation/train.py
@@ -71,7 +71,7 @@ def masked_categorical_crossentropy(gt, pr):
 
 def weighted_categorical_crossentropy(y_true, y_pred):
     
-    weights = [0.005,0.005,0.6,0.195,0.195]
+    weights = [0.005,0.005,0.5,0.195,0.195,0.1]
     Kweights = K.constant(weights)
     
     #if not tf.keras.backend.is_keras_tensor(y_pred): 
